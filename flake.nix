3ac012cef96be6eda365713deca4e857664ac973
Vladimir Serov <me@cab404.ru>
2021-10-13 03:28:50 +0300

    flakes!

--- flake.nix
+++ flake.nix
@@ -1,26 +1,50 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
+        attrs = with builtins; fromJSON (readFile ./package.json);
         pkgs = import nixpkgs { inherit system; };
-        node2nixSources = with pkgs;
-        runCommand "sources" {
-          nativeBuildInputs = [nodePackages.node2nix];
-        } ''
-          cp -r ${./.} $out
-          cd $out
-          node2nix -l
-        '';
       in {
-          defaultPackage = (import node2nixSources { inherit pkgs; }).package;
+          defaultPackage = self.packages.${system}.vsix;
+
+          packages.vsix = pkgs.mkYarnPackage {
+            src = ./.;
+            name = with attrs; "${name}-${version}.vsix";
+            # Not sure how it happened that vscode-direnv symlink gets through .vscodeignore.
+            # But it completely destroys vsce with mystic symlink message, which actually mean that it's gets broken.
+            # Took a long time to solve
+            buildPhase = with attrs; ''
+              rm deps/${name}/${name}
+              yarn vsce package --yarn
+            '';
+            # Nothing needed out there.
+            distPhase = ":";
+            # Just move vsix to result, without any smartness.
+            installPhase = with attrs; ''
+              mv deps/${name}/${name}-${version}.vsix $out
+            '';
+            passthru = {
+              extId = with attrs; "${publisher}.${name}";
+            };
+          };
+
+          packages.extension = let
+            vsix = self.packages.${system}.defaultPackage;
+          in
+          pkgs.vscode-utils.buildVscodeExtension {
+            name = attrs.name;
+            vscodeExtUniqueId = vsix.extId;
+            src = vsix;
+            unpackPhase = "unzip $src; cd extension";
+          };
 
           devShell = with pkgs; mkShell {
-            nativeBuildInputs = [ nodejs ];
+            nativeBuildInputs = [ yarn ];
           };
       }
     );
